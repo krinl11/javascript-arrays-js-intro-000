99ce60e58288ec2dc50eea1d88bf9206de00edd6
Karina Evers <krin_11_bball@yahoo.com>
2019-06-14 20:34:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element){
   return array
 }
 
-function removeElementFromBeginningOfArray(array, element){
+function removeElementFromBeginningOfArray(array){
   array.slice(1)
   return array
 }
