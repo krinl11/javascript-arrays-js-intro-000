7cf4a24596cd9329599be1f5f6d8301c81b94335
Karina Evers <krin_11_bball@yahoo.com>
2019-06-14 20:48:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -42,5 +42,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  return array.slice(removeElementFromEndOfArray.length - 3)
+  return array.slice(0, removeElementFromEndOfArray.length - 1)
 }
